8e28fef178a18775a613359e8630c3e71d7c9381
Ricardo Linck <ricardo.linck@live.com>
2021-04-05 23:44:13 +0100

    Add some endpoints
    Co-authored-by: Rafael Oliveira <rafaellinck.o@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type access struct {
+	barrierIn  string
+	barrierOut string
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	a := make(map[string]*access)
+
+	r.POST("/barrier/:barrierID/in/:registration", func(c *gin.Context) {
+		reg := c.Param("registration")
+		bID := c.Param("barrierID")
+
+		a[reg] = &access{barrierIn: bID}
+
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": fmt.Sprintf("registration-id: %s entered the parking lot using barrier %s", reg, bID),
 		})
 	})
+
+	r.POST("/barrier/:barrierID/out/:registration", func(c *gin.Context) {
+		reg := c.Param("registration")
+		bID := c.Param("barrierID")
+
+		car := a[reg]
+		car.barrierOut = bID
+		delete(a, reg)
+
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("registration-id: %s exited the parking lot using barrier %s", reg, bID),
+		})
+	})
+
+	r.GET("/overview", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("there are %v cars in the parking lot", len(a)),
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
